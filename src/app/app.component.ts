9dc4df117bfa8e9f6e2d526313fced122ae06b2b
Aaron Minnick <abminnick@gmail.com>
2022-06-22 11:45:57 -0700

    add click handler for button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,9 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'pw-generator';
+  onButtonClick() {
+    console.log('Button was clicked');
+  }
 }
